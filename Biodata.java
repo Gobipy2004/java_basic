ef349d73155fa50ea5c1cb09c01abb53dcf35f5a
Gobipy2004 <gobidasan800m@gmail.com>
2025-08-10 08:21:54 +0530

    BIO DATA

---
+++ Biodata.java
@@ -0,0 +1,28 @@
+public class Biodata
+{
+	public static void main(String[]args)
+	{
+		System.out.println("\t\t\tBIO DATA");
+		System.out.println("---------------------------------------------");
+		String name="Gobidasan M";
+		int age=21;
+		String dob="07-10-2004";
+		String place="Mayiladuthurai";
+		String qualification="B.Sc";
+		String stream="Computer Science";
+		float cgpa=6.71f;
+		String skill="java,SQL";
+		long phone=9345686000l;
+		String language="English,Tamil";
+		System.out.println("Name:\t\t"+name);
+		System.out.println("Age:\t\t"+age);
+		System.out.println("D0B:\t\t"+dob);
+		System.out.println("Place:\t\t"+place);
+		System.out.println("Qualification:\t"+qualification);
+		System.out.println("Stream:\t\t"+stream);
+		System.out.println("CGPA:\t\t"+cgpa);
+		System.out.println("Skills:\t\t"+skill);
+		System.out.println("Phone:\t\t"+phone);
+		System.out.println("Languages:\t"+language);
+	}
+}
