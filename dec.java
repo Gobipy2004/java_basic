5c5fe3376407af79f0f0e4d11291b7ec23dc6088
Gobidasan M <gobi.py@gmail.com>
2025-08-11 21:53:26 +0530

    Create dec.java

---
+++ dec.java
@@ -0,0 +1,53 @@
+public class Dec1
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Data entry\n");
+		int a;//declaration
+		String b;
+		long c;
+		String d="Student";//declaration & initialistaion
+		a=1;//initialisation
+		b="Gobidasan";
+		c=1234567890l;
+		System.out.println("id\t:"+a);
+		System.out.println("name\t:"+b);
+		System.out.println("ph\t:"+c);
+		System.out.println("cuurent\t:"+d);
+		a=2;
+		b="Guhan";
+		c=4567891230l;
+		System.out.println("\nid\t:"+a);
+		System.out.println("name\t:"+b);
+		System.out.println("ph\t:"+c);
+		System.out.println("current\t:"+d);
+		a=3;
+		b="Hari";
+		c=9876543120l;
+		System.out.println("\nid\t:"+a);
+		System.out.println("name\t:"+b);
+		System.out.println("ph\t:"+c);
+		System.out.println("current\t:"+d);
+		a=4;
+		b="siva";
+		c=4236519870l;
+		System.out.println("\nid\t:"+a);
+		System.out.println("name\t:"+b);
+		System.out.println("ph\t:"+c);
+		System.out.println("current\t:"+d);
+		a=5;
+		b="durai";
+		c=7894566541l;
+		System.out.println("\nid\t:"+a);
+		System.out.println("name\t:"+b);
+		System.out.println("ph\t:"+c);
+		System.out.println("current\t:"+d);
+		
+	}
+}
+/*
+here i am using data entry by 
+data_type variable_name; declaration (ex :int a;)
+variable_name = value; initialisation (ex: a=1;)
+data_type variable_name = value/express; declaration & initailisation(ex: int a= 1;)
+*/
