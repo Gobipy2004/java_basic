3a649afb4861eb1a7f4ec01a50e36292cb572ca0
Gobidasan M <gobi.py@gmail.com>
2025-08-11 21:59:52 +0530

    Update Literals.java

--- Literals.java
+++ Literals.java
@@ -1,1 +1,16 @@
-
+public class lit
+  {
+    public static void main(String[]args)
+    {
+      int a=1;//integer
+      double b=8.4;//float
+      char c= '$';//single character
+      String d="hello";//string
+      boolean e=true;// true or false
+    }
+  }
+/* literas also called data type 
+data types have two type
+1-non primitive data type
+2-primitive data type
+*/
