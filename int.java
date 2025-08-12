1971b13273384e65e9b9d2b83b0374a0da760796
Gobidasan M <gobi.py@gmail.com>
2025-08-12 09:04:03 +0530

    single line println statement using declaration and initialisation

---
+++ int.java
@@ -0,0 +1,27 @@
+public class Dec3
+{
+	public static void main(String[]args)
+	{
+		System.out.println("\nmark statement:");
+		System.out.println("\n\t\tSemaster even");
+		System.out.println("--------------------------------------------");
+		String name;
+		int year;
+		double mark;
+		name="gobidasan";
+		year=3;
+		mark=66.71;
+		System.out.println("Name \'"+name+"\'\nyear \'"+year+"\'\nmark \'"+mark+"\'");
+		name="charles";
+		year=2;
+		mark=56.33;
+		System.out.println("\nName \'"+name+"\'\nyear \'"+year+"\'\nmark \'"+mark+"\'");
+		name="siva";
+		year=3;
+		mark=89.22;
+		System.out.println("\nName \'"+name+"\'\nyear \'"+year+"\'\nmark \'"+mark+"\'");
+	}
+}
+/*
+here same declaration and initialisation but single line println statement
+*/
