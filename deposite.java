72f1b7209c84ee59e563e13614cca54d2dff4e32
Gobidasan M <gobi.py@gmail.com>
2025-08-14 19:40:09 +0530

    deposite amount using addition operator

---
+++ deposite.java
@@ -0,0 +1,16 @@
+class ram
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\nsample ATM:");
+		System.out.println("*********************************************");
+		double amount=3006.74;
+		float total;
+		System.out.println("initaial amount\t:"+amount);
+		double deposite=5500;
+		System.out.println("after deposit\t:"+deposite);
+		total=(float)amount+(float)deposite;
+		System.out.println("total amount\t:"+total);
+		System.out.println("**********************************************");
+	}
+}
