6900767372a9a9914ea8711f87c0bb32770297ba
Gobidasan M <gobi.py@gmail.com>
2025-08-15 19:27:59 +0530

    Create simple statement by using (*)

---
+++ mul.java
@@ -0,0 +1,23 @@
+class goku
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\nGoku Account Statement:");
+		System.out.println("-----------------------------------------");
+		System.out.println("Agusth Month Statement:");
+		float amount=3000;
+		System.out.println("Initial Amount\t:"+amount);
+		float deposite=4000;
+		System.out.println("Deposite Amount\t:"+deposite);
+		amount=amount+deposite;
+		System.out.println("After Deposite\t:"+amount);
+		int shoes=3;
+		float bill=1500;
+		bill=bill*shoes;
+		System.out.println("Total cost\t:"+bill);
+		amount=amount-bill;
+		System.out.println("Balance\t\t:"+amount);
+		System.out.println("-----------------------------------------");
+		
+	}
+}
