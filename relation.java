d6a889aee25e1da018dbcabf57cbd106e4c83eaa
Gobidasan M <gobi.py@gmail.com>
2025-08-18 18:25:49 +0530

    basic relation operator

---
+++ relation.java
@@ -0,0 +1,21 @@
+class ice
+{
+	public static void main(String[] args)
+	{
+		int a=5;
+		a+=5;
+		System.out.println(a);
+		int b=10;
+		b-=5;
+		System.out.println(b);
+		int c=15;
+		c*=2;
+		System.out.println(c);
+		int d=100;
+		d/=10;
+		System.out.println(d);
+		int e=123;
+		e%=10;
+		System.out.println(e);
+	}
+}
